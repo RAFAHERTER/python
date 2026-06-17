1cfc0121f2ea0de3948a11a2e9b5d643d203421d
Rafael Herter <herter.rafaell@gmail.com>
2026-06-16 22:34:36 -0300

    Aula + testes

---
+++ CursoemVideo/Testes/aula01.py
@@ -0,0 +1,5 @@
+num1=int(input('Digite um valor: '))
+num2=int(input('Digite outro valor: '))
+s=num1+num2
+print('A soma entre {} e {} é {}'.format(num1, num2, s))
+#print('A soma entre', num1, 'e', n2, 'vale', s), ÉRES MUY COMPLICADE!!
