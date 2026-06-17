5bcfde9ff2c29d42c5f67a1f783de72fa3b1d61f
Rafael Herter <herter.rafaell@gmail.com>
2026-06-16 21:20:08 -0300

    Anotações
    Quais são as formas de estabelecer variáveis, e introduzindo a "máscara", com format.

---
+++ CursoemVideo/Prática/pratica.py
@@ -0,0 +1,12 @@
+num1=int(input('Digite um número: '))
+num2=int(input('Digite o segundo número: '))
+soma= num1 + num2
+print('A soma é: {} '.format(soma))
+
+#No python temos 4 tipos de enumerações possíveis a serem colocadas
+#Temos o int, float, bool e str, cada um com as suas representatividades diferentes.
+#No int temos quaisquer valores inteiros, tanto positivos quanto negativos.
+#No float, temos quaisquer valores reais, mesmo que infinitos.
+#No bool, temos valores como verdadeiro (True) e falso (False).
+#No string ou str, é o padrão que nós usamos para palavaras
+
