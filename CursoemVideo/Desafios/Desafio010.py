564ea1e0e271dbf6f93fb066f0a51cc99ffb7464
Rafael Herter <herter.rafaell@gmail.com>
2026-06-18 20:49:18 -0300

    Desafio012

--- CursoemVideo/Desafios/Desafio010.py
+++ CursoemVideo/Desafios/Desafio010.py
@@ -2,5 +2,5 @@ num=float(input('Digite o valor que possuí '
                 'na carteira: '))
 dolar= num/3.27
 print('Você que possuí {} reais na carteira, \n'
-      'convertidos em dólares, você tem {:.2} dólares'
+      'convertidos em dólares, você tem {:.2f} dólares'
       .format(num, dolar))
---
+++ CursoemVideo/Desafios/Desafio012.py
@@ -0,0 +1,4 @@
+num=float(input('Informe um preço para qualquer produto: '))
+desconto= num - (num*5/100)
+print('O preço do produto com o '
+      'desconto é de {:.2f}'.format(desconto))
