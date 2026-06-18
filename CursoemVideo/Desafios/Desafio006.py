04f127ce7831625b94a4a17b091ddc28b02dc1d6
Rafael Herter <herter.rafaell@gmail.com>
2026-06-18 17:29:31 -0300

    Desafio 006

---
+++ CursoemVideo/Desafios/Desafio006.py
@@ -0,0 +1,5 @@
+num=int(input('Digite um número real: '))
+print('O dobro desse número é {} \n '
+      'O triplo desse número é {} \n'
+      'E a raiz quadrada desse número é {}'
+      .format(num*2, num*3, num**(1/2)))
