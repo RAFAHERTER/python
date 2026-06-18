0a92893a4ca78801cced6b6357a35b1b235860a3
Rafael Herter <herter.rafaell@gmail.com>
2026-06-18 20:28:30 -0300

    Desafio010

---
+++ CursoemVideo/Desafios/Desafio010.py
@@ -0,0 +1,6 @@
+num=float(input('Digite o valor que possuí '
+                'na carteira: '))
+dolar= num/3.27
+print('Você que possuí {} reais na carteira, \n'
+      'convertidos em dólares, você tem {:.2} dólares'
+      .format(num, dolar))
