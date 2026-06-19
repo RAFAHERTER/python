c3211753c366eba6a8201425a7f407bc242e6f69
Rafael Herter <herter.rafaell@gmail.com>
2026-06-19 13:23:37 -0300

    Desafio014

---
+++ CursoemVideo/Desafios/Desafio014.py
@@ -0,0 +1,6 @@
+celcius= float(input('Informe a temperatura em'
+                     '°C: '))
+F= 9*celcius/5 + 32
+kelvin = celcius + 273
+print('A temperatura {}°C é equivalente a {}°F'
+      'e a {}K'.format(celcius, F, kelvin))
