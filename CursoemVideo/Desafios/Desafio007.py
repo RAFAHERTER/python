c28e2a2b2616a00b98b92ba1a7612a3b280ac569
Rafael Herter <herter.rafaell@gmail.com>
2026-06-18 21:14:00 -0300

    Ajustes ex007

--- CursoemVideo/Desafios/Desafio007.py
+++ CursoemVideo/Desafios/Desafio007.py
@@ -1,4 +1,4 @@
 nota1=float(input('Digite a primeira nota: '))
 nota2=float(input('Digite a segunda nota: '))
-media = (nota1+nota2)/2
-print('A média do aluno é de {}'.format(media))
+média = (nota1+nota2)/2
+print('A média do aluno é de {:.1f}'.format(média))
