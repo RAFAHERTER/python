e87b84fe839f05ddde1bd023848ff150edb34a7d
Rafael Herter <herter.rafaell@gmail.com>
2026-06-20 19:53:10 -0300

    Aula 04
    Teoria

---
+++ CursoemVideo/Aulas/aula04.py
@@ -0,0 +1,27 @@
+#Para fazer alguns códigos é necessário criar módulos
+#Assim é necessário acessar bibliotecas
+
+#Na linguagem da programação, é necessário realizar importações à essas bibliotecas
+
+#A quantidade de comandos em python é limitada, justamente
+#para evitar códigos extremamentes grandes.
+#por isso é necessário realizar importações para que seja
+#possível realizar outras funções.
+
+#Para incluir algo em python, usamos o comando import.
+#Isso irá resultar na importação de tudo que existe
+#na biblioteca importada. (Mais generalista)
+
+#Para incluir apenas um elemento da biblioteca,
+#basta usar o from 'biblioteca' import 'elemento' , 'outro elemento' (mais específico)
+
+#Na a biblioteca 'math' temos diversas funcionalidades
+#que podem serem usadas, como:
+#ceil - arredondamento pra cima
+#floor - arredondamento pra baixo
+#trunc - eliminar da ',' pra frente os números
+#pow - potência
+#sqrt - Raiz quadrada
+#factorial
+#Entre várias outras funcionalidades.
+
