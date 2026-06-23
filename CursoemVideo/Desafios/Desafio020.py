c63679743dbed064016ab3cd54446b0163b73c48
Rafael Herter <herter.rafaell@gmail.com>
2026-06-23 20:19:29 -0300

    ajustes desafio020

--- CursoemVideo/Desafios/Desafio020.py
+++ CursoemVideo/Desafios/Desafio020.py
@@ -8,5 +8,5 @@ al4=input('Digite o nome do aluno(a): ')
 alunos=[al1,al2,al3,al4]
 random.shuffle(alunos)
 
-print('A ordem de sequência para as apresentações é \n{}; '
-      '\n{}; \n{}; \n{}.'.format(alunos[0], alunos[1], alunos[2], alunos[3]))
+print('A ordem de sequência para as apresentações é: ')
+print(alunos)
