ce5860a6425c1eddf4e34c7cf07f2d18f5376397
Rafael Herter <herter.rafaell@gmail.com>
2026-06-23 20:40:27 -0300

    Desafio021

--- CursoemVideo/Desafios/Desafio021.py
+++ CursoemVideo/Desafios/Desafio021.py
@@ -1,1 +1,3 @@
-import playsound
+import pygame #Infelizmente o pygame ainda não está disponível para essa versão do python, sendo ela a mais morderna até o momento
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
