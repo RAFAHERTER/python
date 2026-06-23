50609f1a00064bc7e96de220db47bd6df1f05ae0
Rafael Herter <herter.rafaell@gmail.com>
2026-06-23 16:31:04 -0300

    Desafio019

---
+++ CursoemVideo/Desafios/Desafio019.py
@@ -0,0 +1,10 @@
+import random
+aluno1=str(input('Digite o nome do primeiro aluno(a): '))
+aluno2=str(input('Digite o nome do segundo aluno(a): '))
+aluno3=str(input('Digite o nome do quarto aluno(a): '))
+aluno4=str(input('Digite o nome do quinto aluno(a): '))
+alunos=[aluno1,aluno2,aluno3,aluno4]
+sorteado=random.choice(alunos)
+
+print('O aluno sorteado para limpar o quadro pro professor é o(a) {}'
+      .format(sorteado))
