74e3f14eacb35c53ee699adfff93cd4b1619d057
Rafael Herter <herter.rafaell@gmail.com>
2026-06-23 20:42:14 -0300

    ajustes

--- CursoemVideo/Desafios/Desafio021.py
+++ CursoemVideo/Desafios/Desafio021.py
@@ -1,3 +1,5 @@
 import pygame #Infelizmente o pygame ainda não está disponível para essa versão do python, sendo ela a mais morderna até o momento
 pygame.init()
 pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play('ex021.mp3')
+pygame.event.wait()
