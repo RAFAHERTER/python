12fb33dc36d9343d03a0ea70b2c4862ef87c0e7f
Rafael Herter <herter.rafaell@gmail.com>
2026-06-23 20:14:30 -0300

    Ajuste desafio 019

--- CursoemVideo/Desafios/Desafio019.py
+++ CursoemVideo/Desafios/Desafio019.py
@@ -3,7 +3,7 @@ aluno1=str(input('Digite o nome do primeiro aluno(a): '))
 aluno2=str(input('Digite o nome do segundo aluno(a): '))
 aluno3=str(input('Digite o nome do quarto aluno(a): '))
 aluno4=str(input('Digite o nome do quinto aluno(a): '))
-alunos=[aluno1,aluno2,aluno3,aluno4]
+alunos=[aluno1,aluno2,aluno3,aluno4] #Criar uma lista no python é feito entre colchetes.
 sorteado=random.choice(alunos)
 
 print('O aluno sorteado para limpar o quadro pro professor é o(a) {}'
