4eba2cf82811d72ae4714007d03da30cacca270d
Rafael Herter <herter.rafaell@gmail.com>
2026-07-01 20:06:30 -0300

    Ajustes
    COM GB

--- CursoemVideo/Desafios/Desafio025.py
+++ CursoemVideo/Desafios/Desafio025.py
@@ -1,2 +1,6 @@
-nome=str(input('Digite seu nome: '))
-print('Silva' in nome.strip().title())
+"""nome=str(input('Digite seu nome: '))
+print('Silva' in nome.strip().title())"""
+
+#----------GOM GB---------
+nome=str(input('Qaul qual seu nome completo? '))
+print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
