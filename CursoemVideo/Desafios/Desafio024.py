09db7ea317abb2ab0c0e243f889860fc49e0ab7e
Rafael Herter <herter.rafaell@gmail.com>
2026-07-01 20:01:01 -0300

    Ajustes
    COM GB

--- CursoemVideo/Desafios/Desafio024.py
+++ CursoemVideo/Desafios/Desafio024.py
@@ -1,2 +1,8 @@
-cidade= str(input('Digite o nome de sua cidade: '))
-print('Santo' in cidade.strip().title())
+"""cidade= str(input('Digite o nome de sua cidade: ')).strip().title().capitalize().lower().upper()
+print('Santo' in cidade.strip().title())"""
+#Errado pois ele reconhece em toda a frase. No enunciado pedia apenas no começo.
+
+#------------COM GUSTAVO GUANABARA-------------
+
+cid= str(input('Qual cidade que você nasceu? ')).strip()
+print(cid[0:5].upper() == 'SANTO')
