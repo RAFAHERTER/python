ff8aab4ced000ef0ee138020ee9ca7f2cfca0f8f
Rafael Herter <herter.rafaell@gmail.com>
2026-07-01 20:46:34 -0300

    Ajustes
    COM GB

--- CursoemVideo/Desafios/Desafio026.py
+++ CursoemVideo/Desafios/Desafio026.py
@@ -1,5 +1,13 @@
-frase= str(input('Digite uma frase: ')).strip().replace(' ', '')
+"""frase= str(input('Digite uma frase: ')).strip().replace(' ', '')
 print(frase)
 print('A letra "a" aparece {} vezes'.format(frase.count('a')))
 print('A primeira letra "a" encontra-se na posição {}'.format(frase.find('a')))
-print('A última letra "a" encontra-se na posição {}'.format(frase.rfind('a')))
+print('A última letra "a" encontra-se na posição {}'.format(frase.rfind('a')))"""
+
+#--------------COM GB----------
+
+frase=str(input('Digite uma frase: ')).upper().strip() #Coloca a frase toda em maiúscula
+print('A letra "A" aparece {} vezes'.format(frase.count('A'))) #Esse +1 está aí, pois as posições de uma string sempre
+#começa com zero "0".
+print('A primeira letra "A" aparece na posição {}'.format(frase.find('A')+1))
+print('A última letra "A" apareceu na posição {}'.format(frase.rfind('A')+1))
