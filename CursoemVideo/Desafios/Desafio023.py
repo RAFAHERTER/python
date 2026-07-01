a52006543d221e36eed5f5e286ee4cbc45124be9
Rafael Herter <herter.rafaell@gmail.com>
2026-07-01 15:03:01 -0300

    Desafio023

--- CursoemVideo/Desafios/Desafio023.py
+++ CursoemVideo/Desafios/Desafio023.py
@@ -1,4 +1,15 @@
+'''num = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor: '))
+num3= num // num2
+resto = num % num2
+print('O resultado da divisão inteira entre {} e {} é igual a {} e resta {}'.format(num,num2,num3, resto))'''
+
 valor= int(input('Digite um número: '))
-print('Unidade ')
+print('Unidades: {}'.format(valor%10))
+print('Dezenas: {}'.format(valor//10 % 10))
+print('Centenas: {}'.format(valor//100 % 10))
+print('Milhares: {}'.format(valor//1000 % 10))
+print('Dezenas de Milhares: {}'.format(valor//10000 % 10))
+print('Centenas de Milhares: {}'.format(valor//100000 % 10))
 
 
