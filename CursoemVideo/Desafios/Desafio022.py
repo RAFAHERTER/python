18af6463e8010d983e40aa118f2f4d19fda908bb
Rafael Herter <herter.rafaell@gmail.com>
2026-06-30 21:44:36 -0300

    Desafio022

---
+++ CursoemVideo/Desafios/Desafio022.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print(nome.upper())
+print(nome.lower())
+print(len(nome.strip()))
+print('O primeiro nome tem {} letras'. format(len(nome.split()[0])))
+print('E o último nome possuí {} letras'.format(len(nome.split()[-1])))
