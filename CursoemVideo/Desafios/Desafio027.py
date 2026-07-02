59e6cb5810f6c9a5209c08e9471794d97c468e62
Rafael Herter <herter.rafaell@gmail.com>
2026-07-02 19:46:02 -0300

    Ajustes
    Com GB

--- CursoemVideo/Desafios/Desafio027.py
+++ CursoemVideo/Desafios/Desafio027.py
@@ -1,3 +1,12 @@
-nome = str(input('Digite seu nome completo: '))
+"""nome = str(input('Digite seu nome completo: ')).strip()
+print('Muito prazer te conhecer {}!!'.format(nome.split()[0]))
 print('O primeiro nome é {}'.format(nome.split()[0]))
-print('E o último nome é {}'.format(nome.split()[-1]))
+print('E o último nome é {}'.format(nome.split()[-1]))"""
+
+#----------COM GB--------------
+
+n = str(input('Digite seu nome completo: ')).strip()
+nome= n.split()
+print('Muito prazer em te conhecer {}!!'.format(nome[0]))
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[-1]))
