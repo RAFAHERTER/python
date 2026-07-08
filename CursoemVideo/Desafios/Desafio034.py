0b52e975b127ffcce569b8236a80173468cb058b
Rafael Herter <herter.rafaell@gmail.com>
2026-07-08 18:34:48 -0300

    Ajustes desafio034

--- CursoemVideo/Desafios/Desafio034.py
+++ CursoemVideo/Desafios/Desafio034.py
@@ -4,8 +4,8 @@ salario = float(input('Digite o seu salario:R$ '))
 if salario >= 1250:
     aumento = salario + (salario * 10 / 100)
     print('O seu salário teve um aumento de 10%\n'
-          'Com resultado final de {}'.format(aumento))
+          'Com resultado final de R${:.2f}'.format(aumento).replace('.',','))
 else:
     aumento = salario + (salario * 15 / 100)
     print('O seu salário teve um aumento de 15%\n'
-          'Com resultado final de {}'.format(aumento))
+          'Com resultado final de R${:.2f}'.format(aumento).replace('.',','))
