65c5db5cd7fb1b739fc72e3c262c5ebc0e5be653
Rafael Herter <herter.rafaell@gmail.com>
2026-07-08 17:57:04 -0300

    Ajustes
    Desafio032

--- CursoemVideo/Desafios/Desafio032.py
+++ CursoemVideo/Desafios/Desafio032.py
@@ -1,6 +1,7 @@
-ano = int(input('Digite um ano qualquer: '))
+ano = int(input('Qual ano você quer analisar? '))
 bi = ano % 4
-if bi == 0:
+
+if bi == 0 and ano % 100 != 0 or ano % 400 == 0: #Anos bissextos não pegam os múltiplos de 100 que não são múltiplos de 400.
     print('O ano {} é bissexto.'.format(ano))
 else:
     print('O ano {} não é bissexto.'.format(ano))
