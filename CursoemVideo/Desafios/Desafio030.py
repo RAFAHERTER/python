235ab3878f3e76d56cdc107b114cd90ba8e8ffba
Rafael Herter <herter.rafaell@gmail.com>
2026-07-08 17:37:41 -0300

    Nada muito especial
    Estava correto

--- CursoemVideo/Desafios/Desafio030.py
+++ CursoemVideo/Desafios/Desafio030.py
@@ -1,5 +1,6 @@
 numero = int(input('Digite um número inteiro: '))
-if numero % 2 == 0:
+resultado = numero % 2
+if resultado == 0:
     print('O número {} é par.'.format(numero))
 else:
     print('O número {} é impar.'.format(numero))
