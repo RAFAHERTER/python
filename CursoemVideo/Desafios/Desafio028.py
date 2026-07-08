2803adaffa0861f5514da92842928dc7e57773cc
Rafael Herter <herter.rafaell@gmail.com>
2026-07-08 17:23:33 -0300

    Ajustes
    Desafio028

--- CursoemVideo/Desafios/Desafio028.py
+++ CursoemVideo/Desafios/Desafio028.py
@@ -1,10 +1,13 @@
-import random
-numero = random.randint(0, 5)
-
-print('Vou pensar em um número entre 0 e 5 \nTente acertar')
-chute= (int(input('Digite aqui o seu chute: ')))
-
+from random import randint
+from time import sleep
+numero = randint(0, 5)
+print('-=-'*20)
+print('Vou pensar em um número entre 0 e 5 Tente acertar...')
+print('-=-'*20)
+chute= (int(input('Em que número eu pensei? ')))
+print('PROCESSANDO...')
+sleep(1) #Pausa o programa fazendo-o esperar por x segundos
 if chute == numero:
-    print('Voce acertou!!')
+    print('PARABÉNS! Você conseguiu me vencer!')
 else:
-    print('Voce errou!!')
+    print('GANHEI!! Eu pensei no número {} e não no {}'.format(numero, chute))
