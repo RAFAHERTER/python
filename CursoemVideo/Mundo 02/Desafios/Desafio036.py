a10fe38b5d5e07a4622e87d5c38eae27d9485406
Rafael Herter <herter.rafaell@gmail.com>
2026-07-10 19:38:17 -0300

    Desafio036

---
+++ CursoemVideo/Mundo 02/Desafios/Desafio036.py
@@ -0,0 +1,13 @@
+from time import sleep
+print('\033[34m-\033[m'*30)
+print('\033[31mBanco de Empréstimos para casas.\033[m')
+print('\033[34m-\033[m'*30)
+casa = float(input('Qual o valor da casa? R$'))
+salario = float(input('Qual o seu salário? '))
+anos = int(input('Quantos anos de financiamento? '))
+mensalidade = casa / (anos * 12)
+if mensalidade <= (salario * 0.3):
+    print('Você \033[1;34m PODE\033[m fazer o financiamento para essa casa.\n'
+          'E terá que pagar a mensalidade de R${:.2f} por {} anos'.format(mensalidade, anos))
+else:
+    print('\033[31mEmpréstimo NEGADO!!\033[m')
