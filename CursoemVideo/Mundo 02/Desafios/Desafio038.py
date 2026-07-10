2e1422074d361ea8a95206588ef1286ef2c59cfd
Rafael Herter <herter.rafaell@gmail.com>
2026-07-10 20:53:00 -0300

    Desafio038

---
+++ CursoemVideo/Mundo 02/Desafios/Desafio038.py
@@ -0,0 +1,9 @@
+print('Comparação de números inteiros')
+num1 = int(input('Digite um número inteiro: '))
+num2 = int(input('Digite outro número inteiro: '))
+if num1 > num2:
+    print('O numero \033[31m{}\033[m é o maior'.format(num1))
+elif num1 < num2:
+    print('O número \033[31m{}\033[m é o maior'.format(num2))
+else:
+    print('Os dois números são \033[31mIGUAIS!')
