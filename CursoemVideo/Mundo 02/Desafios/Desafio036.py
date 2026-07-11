88cca6a806e5c013ecd15958fcb6bde7cc099f21
Rafael Herter <herter.rafaell@gmail.com>
2026-07-11 13:21:53 -0300

    Ajustes
    Desafio 100% CORRETO!!

--- CursoemVideo/Mundo 02/Desafios/Desafio036.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio036.py
@@ -6,6 +6,9 @@ casa = float(input('Qual o valor da casa? R$'))
 salario = float(input('Qual o seu salário? '))
 anos = int(input('Quantos anos de financiamento? '))
 mensalidade = casa / (anos * 12)
+print('Processando...')
+sleep(0.5)
+
 if mensalidade <= (salario * 0.3):
     print('Você\033[1;34m PODE\033[m fazer o financiamento para essa casa.\n'
           'E terá que pagar a mensalidade de\033[1;36m R${:.2f}\033[m por \033[1;35m{}\033[m anos'.format(mensalidade, anos))
