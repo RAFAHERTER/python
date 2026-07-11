13afc89de3cf43313d23740017f0441aa075adb5
Rafael Herter <herter.rafaell@gmail.com>
2026-07-11 14:05:22 -0300

    Ajustes
    TUDO CERTO

--- CursoemVideo/Mundo 02/Desafios/Desafio037.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio037.py
@@ -1,19 +1,19 @@
 num = int(input('Digite um número: '))
 print('Escolha a base de conversão do número digitado '
       'para uma das opções abaixo:\n'
-      'Binário\n'
-      'Octal\n'
-      'Hexadecimal')
-escolha = input('').strip().upper()
+      '[ 1 ] converter para BINÁRIO\n'
+      '[ 2 ] converter para OCTAL\n'
+      '[ 3 ] converter para HEXADECIMAL')
+escolha = int(input('Sua opção: '))
 
-if escolha == 'BINÁRIO' or escolha == 'BINARIO':
-    binario = bin(num)[2:]
+if escolha == 1:
+    binario = bin(num)[2:] #'0b' Para binário
     print('O número {} em binários é {}'.format(num, binario))
-elif escolha == 'OCTAL':
-    octal = oct(num)[2:]
+elif escolha == 2:
+    octal = oct(num)[2:] #'0o' Para octal
     print('O número {} em octal é {}'.format(num, octal))
-elif escolha == 'HEXADECIMAL':
-    hexadecimal = hex(num)[2:]
+elif escolha == 3:
+    hexadecimal = hex(num)[2:] #'0x' Para hexadecimal
     print('O número {} em hexadecimal é {}'.format(num, hexadecimal))
 else:
     print('Opção inválida. Escolha Binário, Octal ou Hexadecimal.')
