8ff612b30b245412c689b7aedb85784b817f8fe7
Rafael Herter <herter.rafaell@gmail.com>
2026-07-10 23:49:53 -0300

    Update Desafio043.py

--- CursoemVideo/Mundo 02/Desafios/Desafio043.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio043.py
@@ -4,10 +4,10 @@ imc = peso / pow(altura,2)
 if 0 < imc <= 18.5:
     print('Seu Índice de Massa Corpórea é {:.2f}'.format(imc))
     print('Abaixo do peso')
-elif 18.5 <= imc < 25:
+elif 18.5 <= imc <= 25:
     print('Seu Índice de Massa Corpórea é {:.2f}'.format(imc))
     print('Peso ideal.')
-elif 25 <= imc < 30:
+elif 25 < imc < 30:
     print('Seu Índice de Massa Corpórea é {:.2f}'.format(imc))
     print('Sobrepeso')
 elif 30 <= imc <= 40:
