dbb44b297d394ccd37a0565671d9d1b8749c80a0
Rafael Herter <herter.rafaell@gmail.com>
2026-07-13 16:52:19 -0300

    Ajustes

--- CursoemVideo/Mundo 02/Desafios/Desafio041.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio041.py
@@ -1,21 +1,20 @@
-ano = int(input('Digite o ano de nascimento: '))
+from datetime import date
+ano = date.today().year
+nascimento = int(input('Digite o ano que nasceu: '))
+idade = ano - nascimento
+print('O atleta tem {} anos'.format(idade))
 if ano > 2026:
     print('-=-' *10, '\033[31mERRO!!\033[m', '-=-' *10)
-idade = 2026 - ano
+
 if idade <= 9:
-    print('Você tem {} anos e é um'.format(idade))
     print('Nadador MIRIM')
-elif 9 < idade <= 14:
-    print('Você tem {} anos e é um'.format(idade))
+elif 9 < idade <= 14: # O '9' NÃO É NECESSÁRIO
     print('Nadador INFANTIL')
-elif 14 < idade <= 19:
-    print('Você tem {} anos e é um'.format(idade))
+elif 14 < idade <= 19: # O '14' NÃO É NECESSÁRIO
     print('Nadador JUNIOR')
-elif 19 < idade <= 20:
-    print('Você tem {} anos e é um'.format(idade))
+elif 19 < idade <= 25:
     print('Nadador SÊNIOR')
-elif idade > 20:
-    print('Você tem {} anos e é um'.format(idade))
+elif idade > 25:
     print('Nadador MASTER')
 else:
     print('-=-' *10, '\033[31mERRO!!\033[m', '-=-' *10)
