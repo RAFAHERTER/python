2e5d8dd836f7be9147c5b47ef45b27b1d0aa8407
Rafael Herter <herter.rafaell@gmail.com>
2026-07-13 17:04:57 -0300

    Ajustes
    Porém já estava correto

--- CursoemVideo/Mundo 02/Desafios/Desafio042.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio042.py
@@ -4,12 +4,12 @@ lado1 = float(input('Digite o valor do primeiro lado: '))
 lado2 = float(input('Digite o valor do segundo lado: '))
 lado3 = float(input('Digite o valor do terceiro lado: '))
 if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
-    print('O triângulo pode ser formado.')
-    if lado1 == lado2 and lado1 == lado3:
-        print('Este triângulo é equilátero.')
+    print('Os segmentos acima podem formar um triângulo ', end='')
+    if lado1 == lado2 == lado3:
+        print('EQUILÁTERO!')
     elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
-        print('Este triângulo é isósceles.')
-    elif lado1 != lado2 and lado1 != lado3 and lado2 != lado3:
-        print('Esse triângulo é escaleno.')
+        print('ISÓSCELES!')
+    elif lado1 != lado2 != lado3 != lado1:
+        print('ESCALENO!')
 else:
     print('O triângulo não pode ser formado.')
