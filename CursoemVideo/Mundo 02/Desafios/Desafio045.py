453d2cac269c503d0fc1560af0a8fe3e1962ec8e
Rafael Herter <herter.rafaell@gmail.com>
2026-07-13 18:28:34 -0300

    Ajustes
    Outro método. Porém gostei melhor do meu

--- CursoemVideo/Mundo 02/Desafios/Desafio045.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio045.py
@@ -1,9 +1,15 @@
-import random
+"""from random import choice
+from time import sleep
 print('Vamos jogar JOKENPÔ')
 lista = ['Pedra', 'Papel', 'Tesoura']
-escolha = random.choice(lista)
+escolha = choice(lista)
 jogador = input('Escreva aqui a sua esolha: ').strip().upper()
-
+print('JO')
+sleep(0.8)
+print('KEN')
+sleep(0.8)
+print('PÔ')
+print('-='*10)
 if jogador == 'PEDRA' and escolha == 'Pedra':
     print('Foi um EMPATE!! \nEu também escolhi Pedra.')
 elif jogador == 'PEDRA' and escolha == 'Papel':
@@ -27,3 +33,52 @@ elif jogador == 'TESOURA' and escolha == 'Papel':
 
 else:
     print('\033[31m----------ERRO!!---------\033[m')
+print('-='*10)
+"""
+from random import randint
+from time import sleep
+#Outra forma de fazer isso com o GB
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+
+print('''Suas opções
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA ''')
+jogador = int(input('Qual a sua jogada? '))
+print('JO')
+sleep(0.8)
+print('KEN')
+sleep(0.8)
+print('PÔ')
+print('-='*11)
+print('Computador jogou {}'.format(itens[computador]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('-='*11)
+if computador == 0:  #Computador jogou PEDRA
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('Jogador VENCEU')
+    elif jogador == 2:
+        print('Jogador PERDEU')
+    else:
+        print('Jogada Invalida')
+elif computador == 1:  #Computador jogou PAPEL
+    if jogador == 0:
+        print('Jogador PERDEU')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('Jogador VENCEU')
+    else:
+        print('Jogada Invalida')
+elif computador == 2:  #Computador jogou TESOURA
+    if jogador == 0:
+        print('Jogador VENCEU')
+    elif jogador == 1:
+        print('Jogador PERDEU')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('Jogada Invalida')
