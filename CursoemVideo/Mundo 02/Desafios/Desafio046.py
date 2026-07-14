a8e0807ec09ee1b570fc380676d26d20cd104d9f
Rafael Herter <herter.rafaell@gmail.com>
2026-07-13 22:10:05 -0300

    Desafio046

---
+++ CursoemVideo/Mundo 02/Desafios/Desafio046.py
@@ -0,0 +1,9 @@
+from time import sleep
+print('-=-'*20)
+print('Contagem regressiva')
+print('-=-'*20)
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print('-=-'*20)
+print('Feliz Ano NOVO!!')
