f1c739f2664074af8936af66ee6d877f1a02dab3
Rafael Herter <herter.rafaell@gmail.com>
2026-07-13 22:05:57 -0300

    Aula09

---
+++ CursoemVideo/Mundo 02/Aulas/Aula09.py
@@ -0,0 +1,35 @@
+"""Nova categoria de estrutura de controle: LAÇOS, REPETIÇÕES OU ITERAÇÃO.
+Os laços são representados para estruturas de repetições
+fazendo com o programa se repita, evitando várias linhas
+com comandos repetitivos.
+
+
+
+
+"""
+
+
+#for c in range (1, 6, 2): #Pula de 2 em 2
+ #   print('Oi')
+#print('FIM')
+
+#for c in range (6, 0, -1): #Caso queira decrescente, '-1'
+   # print(c)
+#print('FIM')
+
+#num = int(input('Digite um valor: '))
+#for c in range (0, num +1, 2):
+    #print(c)
+
+#i = int(input('Início: '))
+#f = int(input('Fim: '))
+#p = int(input('Passo: '))
+#for c in range(i, f+1, p):
+ #   print(c)
+#print('Fim')
+
+s = 0
+for c in range(1, 4):
+    n = int(input('Digite um valor: '))
+    s += n #Ou s = s + n, refere-se ao valor da variável repetidas vezes somadas com os próximos valores a ser inserido.
+print('O somatório de todos os valores foi {}'.format(s))
