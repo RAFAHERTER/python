c79b2e6b255d5457e6f2f58af331b49e6cb9e9d1
Rafael Herter <herter.rafaell@gmail.com>
2026-07-15 19:38:18 -0300

    Ajustes
    Correto

--- CursoemVideo/Mundo 02/Desafios/Desafio048.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio048.py
@@ -1,6 +1,8 @@
-s = 0
-for c in range(1, 500, 2 ):
+soma = 0
+cont = 0
+for c in range(1, 501, 2 ):
     if c % 3 == 0:
-        s = s + c
-        print(c)
-print('O somatório foi de {}'.format(s))
+        cont = cont + 1 #Contar quantos números tem
+        soma = soma + c #Acumulador dos valores.
+
+print('O somatório de todos os {} valores solicitados é {}'.format(cont, soma))
