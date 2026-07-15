43acd44ad4af15310975debc85e36d764778ca92
Rafael Herter <herter.rafaell@gmail.com>
2026-07-14 21:34:56 -0300

    Desafio055
    Lembre-se, o ponto de início é um dos melhores recursos e mais comuns nas linguagens de programação.

---
+++ CursoemVideo/Mundo 02/Desafios/Desafio055.py
@@ -0,0 +1,15 @@
+maior = 0
+menor = 0
+
+for c in range(1, 6):
+    peso = float(input('Digite o peso da {}ª pessoa: (Kg) ').format(c))
+    if c == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso registrado foi {}Kg'.format(maior))
+print('O menor peso registrado foi {}Kg'.format(menor))
