a2574f81ed1abfed18924167510df333ffd5764a
Rafael Herter <herter.rafaell@gmail.com>
2026-07-15 19:39:20 -0300

    Update Desafio048.py

--- CursoemVideo/Mundo 02/Desafios/Desafio048.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio048.py
@@ -4,5 +4,4 @@ for c in range(1, 501, 2 ):
     if c % 3 == 0:
         cont = cont + 1 #Contar quantos números tem
         soma = soma + c #Acumulador dos valores.
-
 print('O somatório de todos os {} valores solicitados é {}'.format(cont, soma))
