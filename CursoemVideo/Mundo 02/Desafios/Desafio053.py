770ff4ce7d629faf3bf49889cc95c6c8576105f4
Rafael Herter <herter.rafaell@gmail.com>
2026-07-15 20:50:46 -0300

    Desafio053
    COM GB (O cara complicou foi tudo)
    Mas o meu estava correto

--- CursoemVideo/Mundo 02/Desafios/Desafio053.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio053.py
@@ -1,9 +1,24 @@
-frase = input('Digite uma frase qualquer: ')
+'''frase = input('Digite uma frase qualquer: ')
 frase2= frase.strip().replace(' ', '').upper()
 inverso = frase2[::-1]
-
-
+print('O inverso da frase {} é {}'.format(frase2, inverso))
 if frase2 == inverso:
-    print('{} é um Palíndromo'.format(frase))
+    print('Isto É um Palíndromo')
 else:
-    print('{} não é um palíndromo'.format(frase))
+    print('Isto NÃO É um palíndromo')
+    '''
+#EXISTE OUTRO JEITO DE FAZER ISSO, SEM O 'FOR', Que na minha concepção, é mais simples.
+#Com Gustavo Guanabara
+#Ele fez basicamente o mesmo que eu, porém ele splitou a frase
+# E também juntou com join. DESSE JEITO:
+frase = input('Digite uma frase: ').strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto)-1, -1, -1):#preciso pegar a última letra, vai até a primeira letra, e o último -1 é pra pegar o inverso
+    inverso += junto[letra]
+if inverso == junto:
+    print('Temos um Palíndromo.')
+else:
+    print('Não é um palíndromo.')
+    
