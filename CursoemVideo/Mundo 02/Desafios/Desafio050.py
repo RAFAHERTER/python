d8c9ef285f552ce8de067bd840c0f229817a35c2
Rafael Herter <herter.rafaell@gmail.com>
2026-07-15 20:06:32 -0300

    Ajustes
    Correto

--- CursoemVideo/Mundo 02/Desafios/Desafio050.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio050.py
@@ -1,6 +1,8 @@
 s = 0
+cont = 0
 for c in range(1,7):
     num = int(input('Digite um valor: '))
     if num % 2 == 0:
         s = s + num
-print('A soma, apenas dos números pares, é {}'.format(s))
+        cont = cont + 1
+print('Você informou {} número pares, e a soma é igual a {}'.format(cont, s))
