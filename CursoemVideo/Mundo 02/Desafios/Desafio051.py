71310434f0f25b3ca208c38aef98bf4075ee9031
Rafael Herter <herter.rafaell@gmail.com>
2026-07-15 20:14:24 -0300

    Ajustes
    Necessário colocar a fórmula do décimo termo

--- CursoemVideo/Mundo 02/Desafios/Desafio051.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio051.py
@@ -1,4 +1,9 @@
-p = int(input('Digite o primeiro termo: '))
-r = int(input('Digite a razão: '))
-for c in range (p,r * 10 , r):
-    print('{}, ' .format(c), end='')
+print('-----'*10)
+print('             PROGRESSÃO ARITIMÉTICA           ')
+print('-----'*10)
+primeiro = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razão: '))
+decimo = primeiro + (10 - 1) * razao #DECIMO termo da PA
+for c in range (primeiro,decimo + razao , razao):
+    print('{} ' .format(c), end='')
+print('ACABOU')
