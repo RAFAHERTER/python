7de23878b15190496cfce9b7e7cdd2165e1163a3
Rafael Herter <herter.rafaell@gmail.com>
2026-07-15 19:30:17 -0300

    Ajustes
    Porém 100% correto

--- CursoemVideo/Mundo 02/Desafios/Desafio047.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio047.py
@@ -1,2 +1,8 @@
-for c in range(1, 50, 2):
-    print(c+1)
+for c in range(1, 51, 2):
+    print(c+1, end = ', ')
+'''
+Outra maneira de fazer:
+for c in range(1, 51):
+    if c % 2 == 0:
+        print(c)
+'''
