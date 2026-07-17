7edf6b17b6c6f4f1cc682ff7038aabc0fc2e6dfb
Rafael Herter <herter.rafaell@gmail.com>
2026-07-17 14:24:52 -0300

    Ajustes Desafio058
    Ajustes feito sozinho.
    Ao olhar APENAS o resultado do GB, foi possível perceber que o meu estava errado.

--- CursoemVideo/Mundo 02/Desafios/Desafio058.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio058.py
@@ -1,12 +1,14 @@
 from random import randint
-print('-=-'*20)
-print('Vou pensar em um número entre 0 e 10')
+print('''Sou seu computador
+Acabei de pensar em um número entre 0 e 10.
+Será que você consegue adivinhar qual foi?''')
+palpite = int(input('Qual o seu palpite? '))
 aleatorio = randint(0, 10)
-palpite = ''
-tentativas = 0
+tentativas = 1
 while palpite != aleatorio:
-    palpite = int(input('Em que número eu pensei? '))
-    if palpite != aleatorio:
-        print('Errou tente novamente! ')
     tentativas += 1
+    if palpite > aleatorio:
+        palpite = int(input('Menos... Tente mais um vez:  '))
+    if palpite < aleatorio:
+        palpite = int(input('Mais... Tente mais um vez:  '))
 print('PARABÉNS!! Você acertou e precisou de {} tentativas'.format(tentativas))
