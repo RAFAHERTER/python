4ff0419e7299b7903cda3f0da0655ea331984d38
Rafael Herter <herter.rafaell@gmail.com>
2026-07-17 14:18:24 -0300

    Ajustes desafio057
    COM GB

--- CursoemVideo/Mundo 02/Desafios/Desafio057.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio057.py
@@ -1,5 +1,11 @@
-frase = ''
+'''frase = ''
 while frase != 'M' and frase != 'F':
     frase = input('Digite seu sexo: ').strip().upper()
     print('Estrutura inválida, tente novamente!')
-print('Seu sexo é {}'.format(frase))
+print('Seu sexo é {}'.format(frase))'''
+#Com GB
+#VALIDAÇÃO DE DADOS
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0] #Só a primeira letra
+while sexo not in 'MF':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo:  ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso!'.format(sexo))
