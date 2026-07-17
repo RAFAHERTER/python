0e07fe9f7a2852ee20445d9ec6f48f742e3fa34c
Rafael Herter <herter.rafaell@gmail.com>
2026-07-17 14:42:18 -0300

    Solução diferente do GB desafio 058

--- CursoemVideo/Mundo 02/Desafios/Desafio058.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio058.py
@@ -12,3 +12,23 @@ while palpite != aleatorio:
     if palpite < aleatorio:
         palpite = int(input('Mais... Tente mais um vez:  '))
 print('PARABÉNS!! Você acertou e precisou de {} tentativas'.format(tentativas))
+
+#Com GB
+#computador = randint(0, 10)
+#print('''Sou seu computador
+#Acabei de pensar em um número entre 0 e 10.
+#Será que você consegue adivinhar qual foi?''')
+#acertou = False
+#palpites = 0
+#while not acertou:
+ #   jogador = int(input('Qual é seu palpite? '))
+ #   palpites += 1
+#    if jogador == computador:
+ #       acertou = True
+#    else:
+ #       if jogador < computador:
+#            print('Mais.. Tente novamente:')
+#        elif jogador > computador:
+#            print('Menos.. Tente novamente:')
+#print('Acertou com {} tentativas. PARABÉNS!'.format(palpites))
+#'''
