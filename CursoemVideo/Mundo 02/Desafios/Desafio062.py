367e9ffa3d371d86b61d400c8ac464b6112f9a63
Rafael Herter <herter.rafaell@gmail.com>
2026-07-18 17:43:47 -0300

    Desafio062
    Refeito (solo)

--- CursoemVideo/Mundo 02/Desafios/Desafio062.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio062.py
@@ -1,4 +1,4 @@
-primeiro = int(input('Primeiro termo: '))
+'''primeiro = int(input('Primeiro termo: '))
 razao = int(input('Insira a razao: '))
 qtt_termo = 0
 termo = 0
@@ -15,3 +15,30 @@ if mais_termo != 0:
         print('{} '.format(termo), end='')
 else:
     print('Programa finalizado com sucesso!')
+'''
+#Refazendo sozinho
+print('Gerador de PA')
+print('-='*10)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+contador = 1
+termo = primeiro
+mais_termo = 1
+
+while contador <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += razao
+    contador += 1
+print('PAUSA')
+while  mais_termo > 0:
+    mais_termo = int(input('Quantos termo você quer mostrar a mais? '))
+    if mais_termo > 0:
+        for c in range(10, 10 + mais_termo):
+            termo += razao
+            contador += 1
+            print('{} -> '.format(termo), end='')
+        print('PAUSA')
+    elif mais_termo == 0:
+        print('Progressão finalizada com {} termos mostrados'.format(contador))
+
+print('FIM')
