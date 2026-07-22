42d24aca02c6c6f494364d70c0b3713796ea109e
Rafael Herter <herter.rafaell@gmail.com>
2026-07-22 12:11:15 -0300

    Desafio064
    Com GB

--- CursoemVideo/Mundo 02/Desafios/Desafio064.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio064.py
@@ -1,3 +1,4 @@
+'''
 num = 0
 contador = 0
 soma = 0
@@ -9,3 +10,13 @@ while num != 999:
 print('Você digitou {} números'.format(contador))
 print('E a soma entre eles é {}'.format(soma))
 print('Fim do programa!')
+'''
+#Com GB
+contador = soma = 0 #De acordo com GB 'num == 0' é uma gambiarra.
+num = int(input('Digite um número [999 para parar]: '))
+while num != 999:
+    contador += 1
+    soma += num
+    num = int(input('Digite um número [999 para parar]: '))
+print('Você digitou {} número e a soma foi {}'.format(contador, soma))
+
