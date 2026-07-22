e2a654eb9ed451c1d3593f6b070938ff8120a8fe
Rafael Herter <herter.rafaell@gmail.com>
2026-07-22 13:10:23 -0300

    Desafio065
    Com GB
    Meu código inicial estava correto, porém ao ver apenas o resultado do GB pensei que poderia estar errado. Existe alguns furos para as regras de validação, por isso pensei que o meu estaria errado. Mas no fim ele não chegou nesse critério.

--- CursoemVideo/Mundo 02/Desafios/Desafio065.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio065.py
@@ -22,4 +22,23 @@ print('A média entre os {} valores é igual a {}'.format(contador, media))
 print('O maior valor é o {}'.format(maior))
 print('O menor valor é o {}'.format(menor))
 '''
-#Com o GB 
+#Com o GB
+resposta = 'S'
+media = soma = contador = maior = menor = 0
+while resposta in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    contador += 1
+    if contador == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resposta = input('Deseja continuar? [S/N] ').upper().strip()[0]
+
+media = soma / contador
+print('Você digitou {} números, e a média foi de {}'.format(contador, media))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
+
