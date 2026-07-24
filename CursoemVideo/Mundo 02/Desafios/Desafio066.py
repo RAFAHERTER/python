bf66a79fcd01208f227dcc8fadd943af384e2c0d
Rafael Herter <herter.rafaell@gmail.com>
2026-07-24 20:49:06 -0300

    Desafio066
    com GB

--- CursoemVideo/Mundo 02/Desafios/Desafio066.py
+++ CursoemVideo/Mundo 02/Desafios/Desafio066.py
@@ -1,10 +1,20 @@
-valor = contador = soma = 0
+'''valor = contador = soma = 0
 while valor != 999:
     valor = int(input('Digite um número (999 para parar): '))
     if valor == 999:
         break
     soma += valor
     contador += 1
-print(f'foram digitados {contador} números \n'
+print(f'Foram digitados {contador} números \n'
       f'e a soma entre eles é {soma}')
+'''
+#Com GB
+soma = contador = 0
+while True:
+    numero = int(input('Digite um valor (999 para parar): '))
+    if numero == 999:
+        break
+    soma += numero
+    contador += 1
+print(f'A soma dos {contador} valores foi {soma}')
 
