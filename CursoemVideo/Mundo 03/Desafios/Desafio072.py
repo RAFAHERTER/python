9a83e743169a020f4c6a0f71bf7ddee3fa352e70
Rafael Herter <herter.rafaell@gmail.com>
2026-07-25 19:35:26 -0300

    Desafio072

---
+++ CursoemVideo/Mundo 03/Desafios/Desafio072.py
@@ -0,0 +1,11 @@
+insira = int(input('Digite um valor entre 0 e 20: '))
+extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze',
+       'Quatorze', 'Quinze', 'Dezesseis', 'Desessete', 'Dezoito', 'Dezenove', 'Vinte')
+while True:
+    if insira < 0 or insira > 20:
+        print('Tente novamente.', end=' ')
+        insira = int(input('Digite um valor entre 0 e 20: '))
+    if 1 < insira < 20:
+        break
+print(f'Você digitou o número {extenso[insira]}')
+
