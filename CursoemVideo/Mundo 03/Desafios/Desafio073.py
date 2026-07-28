d0c98c7dbd655c24fcba89b864e7452a722c0af8
Rafael Herter <herter.rafaell@gmail.com>
2026-07-28 20:28:38 -0300

    Desafio073
    Com GB
    ALL OK

--- CursoemVideo/Mundo 03/Desafios/Desafio073.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio073.py
@@ -1,4 +1,4 @@
-times = ('Palmeiras', 'Flamengo', 'Athletico Paranaense', 'Fluminense', 'Red Bull Bragantino', 'Bahia', 'Corinthians', 'Cruzeiro',
+'''times = ('Palmeiras', 'Flamengo', 'Athletico Paranaense', 'Fluminense', 'Red Bull Bragantino', 'Bahia', 'Corinthians', 'Cruzeiro',
          'Botafogo', 'Coritiba', 'Vitória', 'São Paulo', 'Atlético Mineiro', 'Santos', 'Internacional',
          'Grêmio', 'Vasco da Gama', 'Mirassol', 'Remo', 'Chapecoense')
 chapecoense = times.index('Chapecoense') + 1
@@ -6,4 +6,20 @@ print(f'Lista dos nomes dos atletas em ordem alfebética: {sorted(times)}')
 print(f'Os 5 primeiros colocados são: {times[:5]}')
 print(f'Os 4 últimos são {times[-4:]}')
 print(f'O Chapecoense está na posição {chapecoense}')
+'''
+#Com GB
+print('-='*15)
+
+times = ('Palmeiras', 'Flamengo', 'Athletico Paranaense', 'Fluminense', 'Red Bull Bragantino', 'Bahia', 'Corinthians', 'Cruzeiro',
+         'Botafogo', 'Coritiba', 'Vitória', 'São Paulo', 'Atlético Mineiro', 'Santos', 'Internacional',
+         'Grêmio', 'Vasco da Gama', 'Mirassol', 'Remo', 'Chapecoense')
+print(f'Lista de time do Brasileirão: {times}')
+print('-='*15)
+print(f'Os 5 primeiros são: {times[:5]}') #Letra A
+print('-='*15)
+print(f'Os 4 últimos colocados são: {times[-4:]}') #Letra B
+print('-='*15)
+print(f'Time em ordem alfabética: {sorted(times)}') #Letra C
+print('-='*15)
+print(f'O Chapecoense está na posição {times.index("Chapecoense")+1}') #Letra D
 
