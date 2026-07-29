1c027c75342f4e48fa51e1d21a076523345c65e4
Rafael Herter <herter.rafaell@gmail.com>
2026-07-29 20:40:17 -0300

    Desafio078
    Reajuste com o maior e menor valor digitados mais de uma vez

--- CursoemVideo/Mundo 03/Desafios/Desafio078.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio078.py
@@ -1,7 +1,14 @@
 lista = list()
 for c in range(0, 5):
-    lista.append(int(input('Digite um valor: ')))
-
-print(f'O maior valor digitado é {max(lista)} encontrado na posição {lista.index(max(lista)) + 1}')
-print(f'O menor valor digitado é {min(lista)} encontrado na posição {lista.index(min(lista)) + 1}')
+    lista.append(int(input(f'Digite um valor para a posição {c}: ')))
+print('=-'*40)
+print(f'Você digitou os valores {lista}')
+print(f'O maior valor digitado é {max(lista)} encontrado nas posições ', end='')
+for pos, conteudo in enumerate(lista):
+    if conteudo == max(lista):
+        print(f'{pos}', end='... ')
+print(f'\nO menor valor digitado é {min(lista)} encontrado nas posições ', end='')
+for pos, conteudo in enumerate(lista):
+    if conteudo == min(lista):
+        print(f'{pos}', end='... ')
 
