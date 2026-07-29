4d5c1ab73a11495c7d06996ec82d1f54f386fe0c
Rafael Herter <herter.rafaell@gmail.com>
2026-07-28 21:14:04 -0300

    Desafio076
    COM GB
    Exercício com tupla única

--- CursoemVideo/Mundo 03/Desafios/Desafio076.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio076.py
@@ -1,4 +1,4 @@
-tupla = ('Lápis', 1.75,
+'''tupla = ('Lápis', 1.75,
          'Mochila', 350.00,
          'Borracha', 5.00,
          'Estojo', 25.00,
@@ -14,4 +14,24 @@ print('-'*40)
 for c in range(len(produtos)):
     print(f'{produtos[c]:.<31}R${preco[c]:>7.2f}')
 print('-'*40)
+'''
+#COM GB
+#Obs: Meu exercício está errado!!
+#O ojetivo era com tupla única, e eu fiz com 3
+listagem = ('Lápis', 1.75,
+         'Mochila', 350.00,
+         'Borracha', 5.00,
+         'Estojo', 25.00,
+         'Caderno', 50.00,
+         'Transferidor',7.80,
+         'Livro', 95.40,
+         'Canetas', 50.00)
+print('-'*40)
+print('LISTAGEM DE PREÇOS'.center(40))
+print('-'*40)
+for pos in range(len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<31}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
 
