f358f4e8ee2151dd1c0d38de659335fcbf850078
Rafael Herter <herter.rafaell@gmail.com>
2026-07-28 21:21:33 -0300

    Desafio077
    COM GB

--- CursoemVideo/Mundo 03/Desafios/Desafio077.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio077.py
@@ -1,9 +1,23 @@
-palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso',
-         'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado',
-         'programador', 'futuro')
+'''palavras = ('aprender', 'programar', 'linguagem',
+            'python', 'curso','gratis', 'estudar',
+            'praticar', 'trabalhar', 'mercado',
+            'programador', 'futuro')
 for c in range(len(palavras)):
     print(f'\nNa palavra {palavras[c]} temos ', end='')
     for x in range(len(palavras[c])):
         if palavras[c][x] in 'aeiou':
             print(f'{palavras[c][x]}' , end=' ')
+'''
+#COM GB
+palavras = ('aprender', 'programar', 'linguagem',
+            'python', 'curso','gratis', 'estudar',
+            'praticar', 'trabalhar', 'mercado',
+            'programador', 'futuro')
+for p in palavras:
+    print(f'\nNa palavra {p} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
+#MDS, ele faz parecer fácil e simples kkkkkkkk
+#Demorei muito para pensar nisso.
 
