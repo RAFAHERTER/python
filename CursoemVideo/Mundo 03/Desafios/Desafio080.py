3e6458f99cf4a634c68d4dbfdced3055e977d7ee
Rafael Herter <herter.rafaell@gmail.com>
2026-07-30 16:57:00 -0300

    Update Desafio080.py
    Com GB
    Que absurdo man. Pra conseguir entender isso. PLMDS

--- CursoemVideo/Mundo 03/Desafios/Desafio080.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio080.py
@@ -1,4 +1,4 @@
-lista = list()
+'''lista = list()
 for c in range(0, 5):
     num = int(input(f'Digite o {c + 1}° valor: '))
     if c == 0:
@@ -16,4 +16,23 @@ for c in range(0, 5):
             lista.append(num)
             print(f'Valor adicionado no final da lista')
 print(lista)
+
+'''
+#Com GB
+lista = []
+for c in range(0, 5):
+    num = int(input('Digite um valor: '))
+    if c == 0 or num > lista[-1]:
+        lista.append(num)
+        print('Adicionado ao final da lista')
+    else:
+        posicao = 0
+        while posicao < len(lista): #Vai de 0 até a última posição da lista
+            if num <= lista[posicao]:
+                lista.insert(posicao, num)
+                print(f'Adicionado na posição {posicao} da lista...')
+                break
+            posicao += 1
+print('-='*30)
+print(f'Os valores em ordem foram {lista}')
 
