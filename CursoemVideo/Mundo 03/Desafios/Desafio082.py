e4522d204bada34754b364df5a58c2796feb7b96
Rafael Herter <herter.rafaell@gmail.com>
2026-07-30 17:21:33 -0300

    Update Desafio082.py
    COM GB

--- CursoemVideo/Mundo 03/Desafios/Desafio082.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio082.py
@@ -1,4 +1,4 @@
-lista = list()
+'''lista = list()
 while True:
     lista.append(int(input('Digite um valor: ')))
     escolha = input('Quer continuar? [S/N] ').strip().upper()[0]
@@ -16,4 +16,25 @@ for c in lista:
 print(f'Sua lista completa: {lista}')
 print(f'Sua lista par: {par}')
 print(f'Sua lista impar: {impar}')
+
+'''
+#COM GB
+num = list()
+pares = list()
+impares = list()
+while True:
+    num.append(int(input('Digite um valor: ')))
+    resposta = str(input('Quer continuar? [S/N] '))
+    if resposta in 'Nn':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    else:
+        impares.append(v)
+print('-='*30)
+print(f'A lista completa é {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de impares é {impares}')
+
 
