3f71213b01a6258e468c8e982ee551a927357381
Rafael Herter <herter.rafaell@gmail.com>
2026-07-30 17:37:58 -0300

    Desafio083
    COM GB

--- CursoemVideo/Mundo 03/Desafios/Desafio083.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio083.py
@@ -1,6 +1,24 @@
-frase = input('Diga uma frase: ')
+'''frase = input('Diga uma frase: ')
 if frase.count('(') == frase.count(')'):
     print('Essa expressão é válida')
 elif frase.count('(') == frase.count(')') == 0 or frase.count('(') != frase.count(')'):
     print('Essa expressão não é válida.')
+
+'''
+#COM GB - Utilizando listas(Diferente de minha pessoa)
+expr = str(input('Diga uma expressão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
 
