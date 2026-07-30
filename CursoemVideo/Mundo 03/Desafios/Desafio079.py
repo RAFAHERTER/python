cb088764f6be96a0f213560ee8f918555cbbdab9
Rafael Herter <herter.rafaell@gmail.com>
2026-07-30 15:59:47 -0300

    Update Desafio079.py
    Com GB

--- CursoemVideo/Mundo 03/Desafios/Desafio079.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio079.py
@@ -1,4 +1,4 @@
-lista = list()
+'''lista = list()
 num = 0
 while True:
     num = (int(input('Digite um valor: ')))
@@ -14,4 +14,21 @@ while True:
         break
 lista.sort()
 print(f'Você digitou os valores {lista}')
+
+'''
+#Com GB
+numeros = list()
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    resposta = input('Quer continuar? [S/N] ').strip().upper()[0]
+    if resposta in 'N':
+        break
+print('-='*30)
+numeros.sort()
+print(f'Você digitou os valores {numeros}')
 
