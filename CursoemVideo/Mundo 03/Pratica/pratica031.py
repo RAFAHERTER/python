7deadfa25952aa14229df80408e6498a500956be
Rafael Herter <herter.rafaell@gmail.com>
2026-07-31 19:00:01 -0300

    pratica031

--- CursoemVideo/Mundo 03/Pratica/pratica031.py
+++ CursoemVideo/Mundo 03/Pratica/pratica031.py
@@ -1,6 +1,21 @@
-valores = [1, 2, 3, 2, 4, 5,
-           6, 1, 2, 8, 9, 10, 11]
-
-
+valores = [10, 1, 2, 3, 4, 5, 6, 7]
+seq_atual = []
+melhor_sequencia = []
+maior = num_seq = 0
+for i, c in enumerate(valores):
+    if i == 0:
+        maior = c
+        seq_atual.append(c)
+    else:
+        if c >= maior:
+            seq_atual.append(c)
+            maior = c
+        elif c < maior:
+            seq_atual = list()
+            seq_atual.append(c)
+            maior = c
+        if len(seq_atual) > len(melhor_sequencia):
+            melhor_sequencia = seq_atual
+print(f'A maior sequência de números crescentes é \n{melhor_sequencia} com {len(melhor_sequencia)} elementos')
 
 
