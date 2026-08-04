f16e3d7b1fa1795aadd36793862cb039a81fd989
Rafael Herter <herter.rafaell@gmail.com>
2026-08-04 00:00:03 -0300

    Desafio084
    COM GB

--- CursoemVideo/Mundo 03/Desafios/Desafio084.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio084.py
@@ -6,7 +6,9 @@ A) Quantas pessoas foram cadastradas
 B) Uma listagem com as pessoas mais PESADAS
 
 C) Uma listagem com as pessoas mais LEVES
-'''
+
+#SOZINHO
+
 dados = list()
 lista = list()
 qtt_pessoas = peso = maior = menor = 0
@@ -34,9 +36,40 @@ print(f'Foram cadastradas {qtt_pessoas} pessoas.')
 print(f'O maior peso foi de {maior}. Peso de ', end=' ') # maior peso
 for p in lista:
     if p[1] == maior:
-        print(p[0], end=' ')
+        print(f'[{p[0]}]', end=' ')
 print(f'\nO menor peso foi de {menor}. Peso de ', end=' ') # menor peso
 for p in lista:
     if p[1] == menor:
-        print(p[0], end=' ')
+        print(f'[{p[0]}]', end=' ')
+'''
+#Com GB
+temp = []
+principal = []
+maior = menor = 0
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(principal) == 0:
+        maior = menor = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    principal.append(temp[:])
+    temp.clear()
+    resposta = input('Quer continuar? [S/N]').strip().upper()[0]
+    if resposta in 'N':
+        break
+print('-='*30)
+print(f'Você cadastrou {len(principal)} pessoas')
+print(f'O maior peso foi de {maior}Kg. Peso de ', end='')
+for p in principal:
+    if p[1] == maior:
+        print(f'[{p[0]}]', end=' ')
+print()
+print(f'O menor peso foi de {menor}Kg. Peso de ', end='')
+for p in principal:
+    if p[1] == menor:
+        print(f'[{p[0]}]', end=' ')
 
