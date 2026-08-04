ff398e9b667c5dd469a5292d4bc5c47fe38ba6e9
Rafael Herter <herter.rafaell@gmail.com>
2026-08-04 00:10:55 -0300

    Update Desafio085.py
    Com GB

--- CursoemVideo/Mundo 03/Desafios/Desafio085.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio085.py
@@ -5,7 +5,9 @@ e cadastre-os em uma lista única que
 mantenha separados os valores pares
 e impares. No final mostre os valores pares
 e impares em ordem crescente
-'''
+
+#SOZINHO
+
 completa = [ [] , [] ]
 for c in range(0, 7):
     num = int(input('Digite um valor: '))
@@ -17,4 +19,19 @@ completa[0].sort()
 completa[1].sort()
 print(f'Os valores pares digitados foram: {completa[0]}')
 print(f'Os valores ímpares foram: {completa[1]}')
+'''
+#Com GB
+num = [ [], [] ]
+valor = 0
+for c in range(1, 8):
+    valor = int(input(f'Digite o {c}° valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+print('-='*30)
+num[0].sort()
+num[1].sort()
+print(f'Os valores pares digitados foram {num[0]}')
+print(f'Os valores ímpares  foram {num[1]}')
 
