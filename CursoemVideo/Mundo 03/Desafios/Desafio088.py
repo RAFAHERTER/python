a5c483f2940af4dc7553cc7c3bee5c66ff626874
Rafael Herter <herter.rafaell@gmail.com>
2026-08-04 00:43:23 -0300

    Update Desafio088.py

--- CursoemVideo/Mundo 03/Desafios/Desafio088.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio088.py
@@ -1,7 +1,16 @@
+'''
+Faça um programa que ajude um jogador
+da MEGA SENA a criar palpites. O programa
+vai perguntar quantos jogos serão gerados
+e vai sortear 6 números entre 1 e 60
+para cada jogo, cadastrando tudo em uma
+lista composta.
+
+#SOZINHO
+
 from random import randint
 from time import sleep
 lista = list()
-
 print('='*30)
 print('MEGA SENA!!'.center(30))
 print('='*30)
@@ -11,9 +20,13 @@ for c in range(0,jogo):
     for d in range(0,6):
         aleatorio = randint(1, 60)
         lista.append(aleatorio)
+        lista.sort()
     print(f'Jogo {c+1}: {lista}')
     lista.clear()
     sleep(1)
 print('='*30)
 print('BOA SORTE!'.center(30))
+'''
+#COM GB
+
 
