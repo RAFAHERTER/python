55cdb992a737f96247efdaf10928a68841faeafd
Rafael Herter de jesus <herter.rafaell@gmail.com>
2026-08-05 20:36:45 -0300

    Create pratica036.py

---
+++ CursoemVideo/Mundo 03/Pratica/pratica036.py
@@ -0,0 +1,11 @@
+from random import randint
+matriz = [ [], [], [] ]
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        aleatorio = randint(1, 9)
+        matriz[l].append(aleatorio)
+        
+for l in range(0, 3):
+    for c in range(0, 3):
+    print(matriz[l][c])
