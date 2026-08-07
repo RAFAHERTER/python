ccb918f9f462ebd9888a53ccfb00d2dd0eda1e83
Rafael Herter <herter.rafaell@gmail.com>
2026-08-07 18:06:34 -0300

    Desafio095
    Incompleto

--- CursoemVideo/Mundo 03/Desafios/Desafio095.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio095.py
@@ -1,22 +1,27 @@
+lista = list()
 dicionario= {}
-dicionario['Nome'] = input('Nome do jogador: ')
-dicionario['Jogos'] = int(input('Quantidade de jogos: '))
 gols = []
 soma_gols = 0
-for c in range(0, dicionario['Jogos']):
-    num_gols = int(input(f'Quantos gols na partida {c}: '))
-    gols.append(num_gols)
-    soma_gols += num_gols
-dicionario['Gols'] = gols[:]
-dicionario['Total de gols'] = soma_gols
+while True:
+    dicionario['Nome'] = input('Nome do jogador: ')
+    dicionario['Jogos'] = int(input('Quantidade de jogos: '))
+    for c in range(0, dicionario['Jogos']):
+        num_gols = int(input(f'Quantos gols na partida {c}: '))
+        gols.append(num_gols)
+        soma_gols += num_gols
+    dicionario['Gols'] = gols[:]
+    dicionario['Total de gols'] = soma_gols
+    lista.append(dicionario.copy())
+    gols.clear()
+    escolha = input('Quer continuar? [S/N] ').strip().upper()[0]
+    while escolha not in 'SN':
+        escolha = input('Quer continuar? [S/N] ').strip().upper()[0]
+    if escolha in 'N':
+        break
 print('-='*30)
-print(dicionario)
-print('-=' *30)
-for k, v in dicionario.items():
-    print(f'O campo {k} tem o valor {v}')
-print('-='*30)
-print(f'O jogador {dicionario['Nome']} jogou {dicionario["Jogos"]} partidas.')
-for c in range(0, dicionario['Jogos']):
-    print(f'{"=>":>4} Na partida {c}, fez {dicionario["Gols"][c]} gols.')
-print(f'Foi um total de {soma_gols} gols.')
+print(f'{"cod":}', f'{"nome":>5}', f'{"gols":>10}', f'{"total":>10}')
+print('-'*40)
 
+for pos, c in enumerate(lista):
+    print(pos , f'{c["Nome"]:>5}', f'{c["Gols"]}', f'{c["Total de gols"]:>10}')
+print('-'*40)
