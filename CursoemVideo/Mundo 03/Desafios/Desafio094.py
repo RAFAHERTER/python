d360b626d849b4048716de18bea5bdc3d491737f
Rafael Herter <herter.rafaell@gmail.com>
2026-08-10 16:27:56 -0300

    Desafio094
    COM GB

--- CursoemVideo/Mundo 03/Desafios/Desafio094.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio094.py
@@ -1,3 +1,15 @@
+'''
+ENUNCIADO:
+Crie um programa que leia nome, sexo, idade de várias pessoas
+guardando os dados de cada pessoa num dicionário e todos os
+dicionários numa lista. No final mostre:
+A) Quantas pessoas foram cadastradas
+B) A média de idade.
+C) Uma lista com as mulheres
+D) Uma lista com idade acima da média.
+
+#SOZINHO
+
 lista_completa = list()
 dicionario = dict()
 lista_mulher = list()
@@ -36,3 +48,45 @@ for c in lista_completa:
     print()
 
 print('<< ENCERRADO >>')
+'''
+#COM GB
+galera = list()
+pessoa = dict()
+soma = media = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resposta = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+        if resposta in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resposta == 'N':
+        break
+print('-='*30)
+print(f'Ao todo temos {len(galera)} pessoas cadastradas') #Letra A
+media = soma / len(galera)
+print(f'A média de idade é de {media:5.2f} anos') #Letra B
+print('As mulheres cadastradas forma ', end='') #Letra C
+for p in galera:
+    if p['sexo'] in 'F':
+        print(f'{p["nome"]}', end=' ')
+print()
+print('Lista das pessoas que estão acima da média: ')
+for p in galera:
+    if p['idade'] >= media:
+        print('   ')
+        for k, v in p.items():
+            print(f'{k} = {v};', end=' ')
+        print()
+print('  <<ENCERRADO>>  ')
+
+
