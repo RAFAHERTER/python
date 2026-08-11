08362bcf0e6d6225e29b6f8ca63dba8e37312cab
Rafael Herter <herter.rafaell@gmail.com>
2026-08-11 13:29:15 -0300

    Desafio095
    Com GB

--- CursoemVideo/Mundo 03/Desafios/Desafio095.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio095.py
@@ -1,3 +1,11 @@
+'''
+ENUNCIADO:
+Aprimore o desafio 093 para que funcione com vários jogadores,
+incluindo um sistema de vizualização de detalhes do
+aproveitamento de cada jogador.
+
+#SOZINHO
+
 lista = list()
 dicionario= {}
 gols = []
@@ -39,4 +47,48 @@ while True:
             print(f'No jogo {c} fez {lista[show_dados]["Gols"][c]} gols.')
     print('-'*30)
 print('<< VOLTE SEMPRE >>'.center(30))
-
+'''
+#COM GB
+time = list()
+jogador = dict()
+partidas = list()
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do Jogador: '))
+    tot = int(input(f'Quantas partidas o {jogador["nome"]} jogou? '))
+    partidas.clear()
+    for c in range(0, tot):
+        partidas.append(int(input(f'Quantos gols na partida {c+1}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total']= sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resposta = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+        if resposta in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resposta in 'N':
+        break
+print('-='*30)
+print('cod ', end='')
+for i in jogador.keys(): #CABEÇALHO
+    print(f'{i:<15} ', end='')
+print('-' * 40)
+for k, v in enumerate(time): #DADOS
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15} ', end='')
+    print()
+print('-' * 40)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar) '))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRO!! Não existe jogador com código {busca}!')
+    else:
+        print(f'  --- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}:')
+        for i, g in enumerate(time[busca]["gols"]):
+            print(f'     No jogo {i+1} fez {g} gols.')
+    print('-'*40)
+print(' <<VOLTE SEMPRE>>')
