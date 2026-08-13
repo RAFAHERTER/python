fcee1410aefed38412c27483854c8a1b8fbe9c7d
Rafael Herter <herter.rafaell@gmail.com>
2026-08-13 14:52:54 -0300

    Desafio096
    COM GB

--- CursoemVideo/Mundo 03/Desafios/Desafio096.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio096.py
@@ -4,10 +4,28 @@ Faça um programa que tenha uma função chamada
 retangular (largura e comprimento) e mostre a área
 do terreno.
 
-'''
+#SOZINHO
+
 def area(largura, comprimento):
     a = largura * comprimento
     print(f'A área do terreno é igual a {a}m²')
+
+
 area(float(input('Qual a largura do terreno? (m) ')),
      float(input('Qual a comprimento do terreno? (m) ')))
 
+'''
+#COM GB
+def area(largura, comprimento):
+    a = largura * comprimento
+    print(f'A área de um terreno {largura} x {comprimento} é de {a}m²')
+
+#Programa principal
+print('Controle de Terrenos')
+print('-'*20)
+l = float(input('LARGURA (m): '))
+c = float(input('COMPRIMENTO (m): '))
+area(l,c)
+#No meu código sozinho, não foi utilizado duas variáveis
+#para guardar na memória. Caso fosse necessário,
+#eu criaria sim outras variáveis.
