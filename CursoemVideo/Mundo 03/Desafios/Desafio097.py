439dc80d0b78cb5ddca7d16c30c7b58876812850
Rafael Herter <herter.rafaell@gmail.com>
2026-08-13 15:28:53 -0300

    Desafio097
    Com GB

--- CursoemVideo/Mundo 03/Desafios/Desafio097.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio097.py
@@ -3,9 +3,7 @@ Faça um programa que tenha uma função chamada 'escreva()'
 que receba um texto qualquer como parâmetro e mostre uma
 mensagem com tamanho adaptável.
 
-'''
-
-
+#SOZINHO
 def escreva(frase):
     print('~'*(len(frase) + 3))
     print(frase.center(len(frase) ))
@@ -16,4 +14,17 @@ escreva('   CURSO DE PYTHON NO YOUTUBE')
 escreva('   SEI LA')
 escreva('   OLÁ MUNDO!!')
 
+'''
+#COM GB
+def escreva(msg):
+    tamanho = len(msg) + 4
+    print('~'*tamanho)
+    print(msg.center(tamanho)) #Não tem no vídeo esse comando 'center'
+    print('~'*tamanho)
+
+
+#Programa Principal
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no YouTube')
+escreva('CeV')
 
