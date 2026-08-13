5e5f477a1cce6359a76fe7dd949c7ab629fe07b2
Rafael Herter <herter.rafaell@gmail.com>
2026-08-13 12:04:53 -0300

    Update Desafio099.py

--- CursoemVideo/Mundo 03/Desafios/Desafio099.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio099.py
@@ -1,3 +1,11 @@
+'''
+Faça um programa que tenha uma função chamada 'maio()', que
+receba vários parâmetros com valores inteiros.
+Seu programa tem que analisar todos os valores e dizer
+qual deles é o maior.
+
+'''
+
 from time import sleep
 
 def maior(*valores):
