61df691d192250e0b7ec0733fe139091070aaab9
Rafael Herter <herter.rafaell@gmail.com>
2026-08-17 01:09:01 -0300

    Create Aula017_Funções.pt2.py

---
+++ CursoemVideo/Mundo 03/Aulas/Aula017_Funções.pt2.py
@@ -0,0 +1,32 @@
+'''
+Funções PARTE 2
+
+Tópicos abordados nessa aula:
+
+ - Interactive Help;
+ - docstrings;
+ - Argumentos opcionais;
+ - Escopo de Variáveis;
+ - Retorno de resultados.
+
+1) Interactive Help
+
+Entre no python console e digite 'help()'
+Nele você pode digitar os comandos que podemos usar no python
+E retornará como um manual de como pode usar diversas outras
+funcionalidades para o comando digitado.
+
+Ou, Você pode acessar através do próprio editor de
+código, vulgo neste mesmo local, digitando:
+'help("comando que deseja estudar o manual")'
+
+Ou, é possível também, imprimir na tela o suporte
+iterativo através do comando print("comando que deseja estudar".__doc__)
+Porém aparentemente não está retornando alguns comandos.
+
+
+2) DOCSTRINGS
+'''
+
+
+
