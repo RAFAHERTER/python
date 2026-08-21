9da7b6c84d061a76e9c583837e777881e70bde49
Rafael Herter <herter.rafaell@gmail.com>
2026-08-21 16:24:18 -0300

    Desafio101
    COM GB
    Escopo de IMPORTAÇÃO DE BIBLIOTECA

--- CursoemVideo/Mundo 03/Desafios/Desafio101.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio101.py
@@ -1,3 +1,15 @@
+'''
+
+Enunciado:
+Crie um programa que tenha uma função chamada voto() que vai
+receber como parâmetro o ano de nascimento de uma pessoa,
+retornando uma valor literal indicando se uma pessoa tem
+voto NEGADO, OPCIONAL ou OBRIGATIO nas eleições
+
+Será falado nesse exercício sobre ESCOPO DE IMPORTAÇÃO
+
+#Sozinho
+
 from datetime import date
 def voto(ano):
     v = date.today().year - ano
@@ -13,4 +25,20 @@ def voto(ano):
 #Programa Principal
 a = int(input('Digite o ano de nascimento: '))
 voto(a)
+'''
 
+
+def voto(ano):
+    from datetime import date #Posso importar apenas para dentro da função, economizando a memória.
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com idade {idade} VOTO NEGADO'
+    elif 16 <= idade < 18 or idade >= 65:
+        return f'Com idade {idade} VOTO OPCIONAL'
+    else:
+        return f'Com idade {idade} VOTO OBRIGATIO'
+
+#Programa Principal
+nascimento = int(input('Digite o ano de nascimento: '))
+print(voto(nascimento))
