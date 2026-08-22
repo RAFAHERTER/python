a9a74a12432fd4d528dfc213e3e438dc9fac6ac6
Rafael Herter <herter.rafaell@gmail.com>
2026-08-22 14:23:08 -0300

    Desafio104

--- CursoemVideo/Mundo 03/Desafios/Desafio104.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio104.py
@@ -1,3 +1,12 @@
+'''
+ENUNCIADO:
+Crie um programa que tenha função' leiaint() ', que vai funcionar
+de forma semelhante à função 'input()' do python, só que
+fazendo a validação para aceitar apenas um valor numérico.
+Exemplo:
+n = leiaint('Digite um número inteiro: ')
+
+#Sozinho
 def leiaInt(msg):
     global n
     num = False
@@ -5,10 +14,29 @@ def leiaInt(msg):
         n = input(msg)
         num = n.isnumeric()
         if num is False:
-            print('ERRO, Digite apenas um número inteiro')
+            print('\033[31mERRO, Digite apenas um número inteiro\033[m')
         else:
             return n
 
 n = leiaInt('Digite um número inteiro: ')
+print(f'Você acabou de digitar o número {n}')
+'''
+#Com GB
+def leiaint(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[31mERRO! Digite um número inteiro válido\033[m')
+        if ok:
+            break
+    return valor
+
+#Programa Principal
+n = leiaint('Digite um número: ')
 print(f'Você acabou de digitar o número {n}')
 
