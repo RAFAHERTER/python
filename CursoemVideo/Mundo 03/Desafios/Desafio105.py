fa4d2939a89e07324654842714761e8e46dc09e5
Rafael Herter <herter.rafaell@gmail.com>
2026-08-22 16:21:38 -0300

    Desafio105
    COM GB

--- CursoemVideo/Mundo 03/Desafios/Desafio105.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio105.py
@@ -1,3 +1,17 @@
+'''
+Enunciado:
+Faça um programa que tenha uma função 'notas()' que pode
+receber várias notas de alunos e vai retornar um dicionário
+com as seguintes informações:
+- Quantidade de notas
+- A maior nota
+- A menor nota
+- A média da turma
+- A situação (opcional)
+Adicione, também, as docstrings
+
+#SOZINHO
+
 def notas(*nota, sit=False):
     """
 Funçõs para analisar notas e situações de vário alunos
@@ -7,13 +21,12 @@ Funçõs para analisar notas e situações de vário alunos
     """
     print('-' * 30)
     dicionario = dict()
-    total = len(dicionario)
     media = sum(nota) / len(nota)
-    dicionario['total'] = total
+    dicionario['total'] = len(dicionario)
     dicionario['maior'] = max(nota)
     dicionario['menor'] = min(nota)
     dicionario['media'] = f'{media:.2f}'
-    if sit == True:
+    if sit:
         if 7 <=  media <= 10:
             dicionario['Situação'] = 'Boa'
         elif media >= 5:
@@ -22,6 +35,33 @@ Funçõs para analisar notas e situações de vário alunos
             dicionario['Situação'] = 'Ruim'
     return dicionario
 
-
+#Programa Principal
 resp = notas(5.5, 7, 6, 2, sit = True)
+print(resp)'''
+
+#Com GB
+def notas(*n, sit = False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: Uma ou mais notas dos alunos (aceita várias)
+    :param sit:Valor opcional, indicando se deve ou não adicionar a situação
+    :return: Dicionário com informações analisadas pelo programa
+    """
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['media'] = sum(n)/len(n)
+    if sit:
+        if r['media'] >= 7:
+            r['Situação'] = 'Boa'
+        elif r['media'] >= 5:
+            r['Situação'] = 'Razoável'
+        else:
+            r['Situação'] = 'Ruim'
+    return r
+
+#Programa Principal
+resp = notas(5.5, 2.5, 9, 8.5, sit = True)
 print(resp)
+help(notas)
