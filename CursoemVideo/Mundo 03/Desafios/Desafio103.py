887d9569f3038aa177662a50b92b5245c52b6e8c
Rafael Herter <herter.rafaell@gmail.com>
2026-08-22 14:09:00 -0300

    Desafio101
    COM GB
    Corrigindo erros anteriores

--- CursoemVideo/Mundo 03/Desafios/Desafio103.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio103.py
@@ -1,3 +1,12 @@
+"""
+Enunciado:
+Faça um programa que tenha uma função chamada 'ficha()', que
+receba dois parâmetros opcionais: o nome de um jogador e
+quantos gols ele marcou. O programa deverá ser capaz de
+mostrar a ficha do jogador, mesmo que algum dado não tenha
+sido informado corretamente.
+
+#Sozinho - Está errado... Validação de entrada não deu certo para os gols.
 def ficha(nome = '', gols = 0):
     if nome == '':
         nome = '<desconhecido>'
@@ -11,5 +20,21 @@ if qtt > 0:
     ficha(jogador, qtt)
 elif qtt == '':
     ficha(jogador, 0)
+"""
+#Com GB
+def ficha(jog = '<desconhecido>', gol=0):
+    print(f'O jogador {jog} fez {gol} gol(s) no campeonato')
 
+
+#Programa Principal
+n = str(input('Nome do jogador: '))
+g = str(input('Quantidade de gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n, g)
+#Solução bem eficiente
 
