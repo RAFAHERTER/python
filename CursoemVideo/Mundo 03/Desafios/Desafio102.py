9257801fea47e027a4767f7ea10e323f41d175ed
Rafael Herter <herter.rafaell@gmail.com>
2026-08-22 13:49:39 -0300

    Desafio102

--- CursoemVideo/Mundo 03/Desafios/Desafio102.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio102.py
@@ -1,3 +1,14 @@
+'''
+ENUNCIADO:
+Crie um programa que tenha uma função fatorial() que receba
+dois parâmetros: o primeiro que indique o número a calcular
+e o outro chamado 'show', que será um valor lógico(opcional)
+indicando se será mostrado ou não na tela o processo de cálculo
+do fatorial.
+
+#Sozinho
+obs: O meu programa foi feito mais aprimorado, com o objetivo
+de ler as variáveis, ao invés de trocar nos códigos.
 def fatorial(numero, show=0 ):
     """
     -> Calcule o Fatorial de um número
@@ -27,4 +38,28 @@ if s == 'N':
 elif s == 'S':
     s = True
 fatorial(n, bool(s))
+
+'''
+#Com GB
+def fatorial(n, show = False):
+    """
+        -> Calcule o Fatorial de um número
+        :param n: O número a ser calculado
+        :param show: (opcional) Mostrar ou não a conta do fatorial
+        :return: O valor do Fatorial de um número 'n'
+        """
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+
+#Programa Principal
+print(fatorial(5, show = True))
+
 
