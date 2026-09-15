6a95bf730108411ddacb5fb20db943d029a95b83
Rafael Herter <herter.rafaell@gmail.com>
2026-09-14 23:47:25 -0300

    Desafio112
    Com alguns ajustes

--- CursoemVideo/Mundo 03/Desafios/Pasta_Desafios/dados/dinheiro.py
+++ CursoemVideo/Mundo 03/Desafios/Pasta_Desafios/dados/dinheiro.py
@@ -1,11 +1,14 @@
 def leiaDinheiro(msg):
-    while True:
-        valor = input(msg).strip().replace(',', '.')
+    valido = False
+    while not valido:
+        valor = str(input(msg)).strip().replace(',', '.')
         if valor.isalpha() or valor == '':
             print(f'\033[0;31mERRO: "{valor}" é um valor inválido!\033[m')
         else:
+            valido = True
             return float(valor)
 
+# A seguir é uma redundância, pois a função leiaDinheiro já faz a validação de entrada de dados. No entanto, a função leiaFloat foi mantida para fins de compatibilidade com o código existente.
 def leiaFloat(msg):
     while True:
         valor = input(msg).strip().replace(',', '.')
