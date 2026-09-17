578e5fc71706b1d1464ee362c6bf385ef265ef6b
Rafael Herter <herter.rafaell@gmail.com>
2026-09-17 14:21:12 -0300

    Indo, mas quebrando a cabeça

--- CursoemVideo/Mundo 03/Desafios/Desafio115.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio115.py
@@ -4,5 +4,3 @@ Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu
 nome e idade em um arquivo de texto simples. O sistema só vai ter 2 opções:
 Cadastrar uma nova pessoa e listar todas as pessoas cadastradas.
 '''
-from uteis import number
-num = number.leiaInt('Digite um número: ')
--- CursoemVideo/Mundo 03/Desafios/Desafio115/menu/sistema.py
+++ CursoemVideo/Mundo 03/Desafios/Desafio115/menu/sistema.py
@@ -1,4 +1,7 @@
-import number
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from number.numeros import leiaInt
 def opcoes():
     print('-'*50)
     print('MENU PRINCIPAL'.center(50))
@@ -9,3 +12,5 @@ def opcoes():
     """)
     print('='*50)
     escolha = leiaInt('Sua opção: ')
+    return escolha
+
