28f4b8859ef0391049865468699b6f4b2c9e16b2
Rafael Herter <herter.rafaell@gmail.com>
2026-09-17 09:28:28 -0300

    Desafio114

---
+++ CursoemVideo/Mundo 03/Desafios/Desafio114.py
@@ -0,0 +1,13 @@
+'''Crie um código em python que teste se o site pudim está acessível pelo 
+computador do usuário.'''
+import requests
+url = 'http://www.pudim.com.br'
+
+try:
+    response = requests.get(url, timeout= 5)
+    if response.status_code == 200:
+        print('O site está acessível!')
+    else:
+        print('O site não está acessível. Código de status:', response.status_code)
+except:
+    print('O site não está acessível. Ocorreu um erro ao tentar acessar o site.')
